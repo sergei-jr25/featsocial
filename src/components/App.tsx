c610f64eca994e6517296b11ec14695acec28022
Ваше Имя <.email@example.com>
2024-11-24 01:51:38 +0300

    eee

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,7 +6,7 @@ import Layout from './layout/Layout'
 const App: FC = () => {
 	return (
 		<Layout>
-			<h1>Hi my name Is Slim Shady</h1>
+			<h1>Hi my name Is Slim Shady eeeee</h1>
 			<Main />
 		</Layout>
 	)
